0b36dabf4df861030c6b12aa30d8cac34f43eb8d
seonghwa.jeong <seonghwa.jeong@amplitude.com>
2025-03-17 17:42:06 +0900

    CSS & 문구 변경

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
-document.getElementById("changeText").addEventListener("click", function() {
-    document.getElementById("text").textContent = "문장이 변경되었습니다!";
-});
+(function() {
+    'use strict';
+    const AMPLITUDE_API_KEY = '94882b869e10cc7647ec3a4fff72715f'; // Your Amplitude Project
+    const options = {
+        amplitudeBundleUrl: `https://cdn.amplitude.com/script/${AMPLITUDE_API_KEY}.js`,
+        engagementScriptUrl: `https://cdn.amplitude.com/script/${AMPLITUDE_API_KEY}.engagement.js`,
+        experimentScriptUrl: `https://cdn.amplitude.com/script/${AMPLITUDE_API_KEY}.experiment.js`,
+        projectAPIKey: AMPLITUDE_API_KEY,
+        amplitudeConfig: {
+            fetchRemoteConfig: true,
+            defaultTracking: {
+                pageViews: true,
+                sessions: true,
+                fileDownloads: true,
+                formInteractions: true,
+                clicks: true,
+                scrolls: true,
+            },
+        }
+    };
+    function importExternal(url) {
+        return new Promise((resolve, reject) => {
+            const script = document.createElement("script");
+            script.src = url;
+            script.async = true;
+            script.onload = () => resolve(window.amplitude);
+            script.onerror = reject;
+            var a = document.getElementsByTagName("script")[0];
+            if (a) {
+                a.parentNode.insertBefore(script, a);
+            }
+        });
+    }
+    Promise.all([
+        importExternal(options.amplitudeBundleUrl),
+        importExternal(options.engagementScriptUrl),
+        importExternal(options.experimentScriptUrl)
+    ]).then(() => {
+        console.log('Amplitude unified script and engagement script loaded');
+        window.amplitude.add(window.sessionReplay.plugin());
+        console.log('Amplitude session replay added');
+        window.amplitude.add(window.engagement.plugin());
+        console.log('Amplitude engagement plugin added');
+        window.amplitude.init(options.projectAPIKey, options.amplitudeConfig);
+        console.log('Amplitude initialized with autocapture options');
+    });
+})();
